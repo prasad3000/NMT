d3cbbe6165e65c02a52aa46874df7e1197274b11
BHUYAN KAIBALYA PRASAD <30308576+prasad3000@users.noreply.github.com>
2019-08-28 18:41:11 +0500

    Add files via upload

---
+++ NMT_fra_eng.py
@@ -0,0 +1,451 @@
+import unicodedata
+import string
+import re
+import random
+import time
+import datetime
+import math
+
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+from torch import optim
+import torch.nn.functional as F
+from torch.nn.utils.rnn import pad_packed_sequence, pack_padded_sequence#, masked_cross_entropy
+from masked_cross_entropy import *
+
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+import numpy as np
+
+
+### Loading the data
+
+# Indexing
+PAD_token = 0
+SOS_token = 1
+EOS_token = 2
+
+class Lang:
+    def __init__(self, name):
+        self.name = name
+        self.trimmed = False
+        self.word2index = {}
+        self.word2count = {}
+        self.index2word = {0: "PAD", 1: "SOS", 2: "EOS"}
+        self.n_words = 3 # Count default tokens
+
+    def index_words(self, sentence):
+        for word in sentence.split(' '):
+            self.index_word(word)
+
+    def index_word(self, word):
+        if word not in self.word2index:
+            self.word2index[word] = self.n_words
+            self.word2count[word] = 1
+            self.index2word[self.n_words] = word
+            self.n_words += 1
+        else:
+            self.word2count[word] += 1
+
+    # Remove words below a certain count threshold
+    def trim(self, min_count):
+        if self.trimmed: return
+        self.trimmed = True
+        
+        keep_words = []
+        
+        for k, v in self.word2count.items():
+            if v >= min_count:
+                keep_words.append(k)
+
+        print('keep_words %s / %s = %.4f' % (
+            len(keep_words), len(self.word2index), len(keep_words) / len(self.word2index)
+        ))
+
+        # Reinitialize dictionaries
+        self.word2index = {}
+        self.word2count = {}
+        self.index2word = {0: "PAD", 1: "SOS", 2: "EOS"}
+        self.n_words = 3 # Count default tokens
+
+        for word in keep_words:
+            self.index_word(word)
+            
+## Reading and decoding files
+            
+# Turn a Unicode string to plain ASCII
+def unicode_to_ascii(s):
+    return ''.join(
+        c for c in unicodedata.normalize('NFD', s)
+        if unicodedata.category(c) != 'Mn')
+
+# Lowercase, trim, and remove non-letter characters
+def normalize_string(s):
+    s = unicode_to_ascii(s.lower().strip())
+    s = re.sub(r"([,.!?])", r" \1 ", s)
+    s = re.sub(r"[^a-zA-Z,.!?]+", r" ", s)
+    s = re.sub(r"\s+", r" ", s).strip()
+    return s
+
+def read_langs(lang1, lang2, reverse=False):
+    print("Reading lines...")
+
+    # Read the file and split into lines
+#     filename = '../data/%s-%s.txt' % (lang1, lang2)
+    filename = '%s_%s.txt' % (lang1, lang2)
+    lines = open(filename).read().strip().split('\n')
+
+    # Split every line into pairs and normalize
+    pairs = [[normalize_string(s) for s in l.split('\t')] for l in lines]
+
+    # Reverse pairs, make Lang instances
+    if reverse:
+        pairs = [list(reversed(p)) for p in pairs]
+        input_lang = Lang(lang2)
+        output_lang = Lang(lang1)
+    else:
+        input_lang = Lang(lang1)
+        output_lang = Lang(lang2)
+
+    return input_lang, output_lang, pairs
+
+
+MIN_LENGTH = 3
+MAX_LENGTH = 25
+
+def filter_pairs(pairs):
+    filtered_pairs = []
+    for pair in pairs:
+        if len(pair[0]) >= MIN_LENGTH and len(pair[0]) <= MAX_LENGTH \
+            and len(pair[1]) >= MIN_LENGTH and len(pair[1]) <= MAX_LENGTH:
+                filtered_pairs.append(pair)
+    return filtered_pairs
+
+
+## prepare the data
+def prepare_data(lang1_name, lang2_name, reverse=False):
+    input_lang, output_lang, pairs = read_langs(lang1_name, lang2_name, reverse)
+    print("Read %d sentence pairs" % len(pairs))
+    
+    pairs = filter_pairs(pairs)
+    print("Filtered to %d pairs" % len(pairs))
+    
+    print("Indexing words...")
+    for pair in pairs:
+        input_lang.index_words(pair[0])
+        output_lang.index_words(pair[1])
+    
+    print('Indexed %d words in input language, %d words in output' % (input_lang.n_words, output_lang.n_words))
+    return input_lang, output_lang, pairs
+
+input_lang, output_lang, pairs = prepare_data('eng', 'fra', True)
+
+
+## Filtering vocab
+MIN_COUNT = 5
+
+input_lang.trim(MIN_COUNT)
+output_lang.trim(MIN_COUNT)
+
+# Filtering pairs
+keep_pairs = []
+
+for pair in pairs:
+    input_sentence = pair[0]
+    output_sentence = pair[1]
+    keep_input = True
+    keep_output = True
+    
+    for word in input_sentence.split(' '):
+        if word not in input_lang.word2index:
+            keep_input = False
+            break
+
+    for word in output_sentence.split(' '):
+        if word not in output_lang.word2index:
+            keep_output = False
+            break
+
+    # Remove if pair doesn't match input and output conditions
+    if keep_input and keep_output:
+        keep_pairs.append(pair)
+
+print("Trimmed from %d pairs to %d, %.4f of total" % (len(pairs), len(keep_pairs), len(keep_pairs) / len(pairs)))
+pairs = keep_pairs
+
+
+
+### Turning training data into tensor
+
+
+# Return a list of indexes, one for each word in the sentence, plus EOS
+def indexes_from_sentence(lang, sentence):
+    return [lang.word2index[word] for word in sentence.split(' ')] + [EOS_token]
+
+
+# Pad a with the PAD symbol
+def pad_seq(seq, max_length):
+    seq += [PAD_token for i in range(max_length - len(seq))]
+    return seq
+
+
+def random_batch(batch_size):
+    input_seqs = []
+    target_seqs = []
+
+    # Choose random pairs
+    for i in range(batch_size):
+        pair = random.choice(pairs)
+        input_seqs.append(indexes_from_sentence(input_lang, pair[0]))
+        target_seqs.append(indexes_from_sentence(output_lang, pair[1]))
+
+    # Zip into pairs, sort by length (descending), unzip
+    seq_pairs = sorted(zip(input_seqs, target_seqs), key=lambda p: len(p[0]), reverse=True)
+    input_seqs, target_seqs = zip(*seq_pairs)
+    
+    # For input and target sequences, get array of lengths and pad with 0s to max length
+    input_lengths = [len(s) for s in input_seqs]
+    input_padded = [pad_seq(s, max(input_lengths)) for s in input_seqs]
+    target_lengths = [len(s) for s in target_seqs]
+    target_padded = [pad_seq(s, max(target_lengths)) for s in target_seqs]
+
+    # Turn padded arrays into (batch_size x max_len) tensors, transpose into (max_len x batch_size)
+    input_var = Variable(torch.LongTensor(input_padded)).transpose(0, 1)
+    target_var = Variable(torch.LongTensor(target_padded)).transpose(0, 1)
+    
+    input_var = input_var.cuda()
+    target_var = target_var.cuda()
+        
+    return input_var, input_lengths, target_var, target_lengths
+
+
+random_batch(2)
+
+
+### Building the model
+
+## The encoder
+
+class EncoderRNN(nn.Module):
+    def __init__(self, input_size, hidden_size, n_layers=1, dropout=0.1):
+        super(EncoderRNN, self).__init__()
+        
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.n_layers = n_layers
+        self.dropout = dropout
+        
+        self.embedding = nn.Embedding(input_size, hidden_size)
+        self.gru = nn.GRU(hidden_size, hidden_size, n_layers, dropout=self.dropout, bidirectional=True)
+        
+    def forward(self, input_seqs, input_lengths, hidden=None):
+        # Note: we run this all at once (over multiple batches of multiple sequences)
+        embedded = self.embedding(input_seqs)
+        packed = torch.nn.utils.rnn.pack_padded_sequence(embedded, input_lengths)
+        outputs, hidden = self.gru(packed, hidden)
+        outputs, output_lengths = torch.nn.utils.rnn.pad_packed_sequence(outputs) # unpack (back to padded)
+        outputs = outputs[:, :, :self.hidden_size] + outputs[:, : ,self.hidden_size:] # Sum bidirectional outputs
+        return outputs, hidden
+    
+
+## Attention Decoder
+class Attn(nn.Module):
+    def __init__(self, method, hidden_size):
+        super(Attn, self).__init__()
+        
+        self.method = method
+        self.hidden_size = hidden_size
+        
+        if self.method == 'general':
+            self.attn = nn.Linear(self.hidden_size, hidden_size)
+
+        elif self.method == 'concat':
+            self.attn = nn.Linear(self.hidden_size * 2, hidden_size)
+            self.v = nn.Parameter(torch.FloatTensor(1, hidden_size))
+
+    def forward(self, hidden, encoder_outputs):
+        max_len = encoder_outputs.size(0)
+        this_batch_size = encoder_outputs.size(1)
+
+        # Create variable to store attention energies
+        attn_energies = Variable(torch.zeros(this_batch_size, max_len)) # B x S
+
+        attn_energies = attn_energies.cuda()
+            
+
+        # For each batch of encoder outputs
+        for b in range(this_batch_size):
+            # Calculate energy for each encoder output
+            for i in range(max_len):
+                attn_energies[b, i] = self.score(hidden[:, b], encoder_outputs[i, b].unsqueeze(0))
+
+        # Normalize energies to weights in range 0 to 1, resize to 1 x B x S
+        return F.softmax(attn_energies).unsqueeze(1)
+    
+    def score(self, hidden, encoder_output):
+        
+        if self.method == 'dot':
+            energy = hidden.dot(encoder_output)
+            return energy
+        
+        elif self.method == 'general':
+            energy = self.attn(encoder_output)
+            energy = hidden.dot(energy)
+            return energy
+        
+        elif self.method == 'concat':
+            energy = self.attn(torch.cat((hidden, encoder_output), 1))
+            energy = self.v.dot(energy)
+            return energy
+        
+## Implementing the Bahdanau et al. model
+            
+class BahdanauAttnDecoderRNN(nn.Module):
+    def __init__(self, hidden_size, output_size, n_layers=1, dropout_p=0.1):
+        super(BahdanauAttnDecoderRNN, self).__init__()
+        
+        # Define parameters
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.n_layers = n_layers
+        self.dropout_p = dropout_p
+        self.max_length = max_length
+        
+        # Define layers
+        self.embedding = nn.Embedding(output_size, hidden_size)
+        self.dropout = nn.Dropout(dropout_p)
+        self.attn = Attn('concat', hidden_size)
+        self.gru = nn.GRU(hidden_size, hidden_size, n_layers, dropout=dropout_p)
+        self.out = nn.Linear(hidden_size, output_size)
+    
+    def forward(self, word_input, last_hidden, encoder_outputs):
+        # Note: we run this one step at a time
+        # TODO: FIX BATCHING
+        
+        # Get the embedding of the current input word (last output word)
+        word_embedded = self.embedding(word_input).view(1, 1, -1) # S=1 x B x N
+        word_embedded = self.dropout(word_embedded)
+        
+        # Calculate attention weights and apply to encoder outputs
+        attn_weights = self.attn(last_hidden[-1], encoder_outputs)
+        context = attn_weights.bmm(encoder_outputs.transpose(0, 1)) # B x 1 x N
+        context = context.transpose(0, 1) # 1 x B x N
+        
+        # Combine embedded input word and attended context, run through RNN
+        rnn_input = torch.cat((word_embedded, context), 2)
+        output, hidden = self.gru(rnn_input, last_hidden)
+        
+        # Final output layer
+        output = output.squeeze(0) # B x N
+        output = F.log_softmax(self.out(torch.cat((output, context), 1)))
+        
+        # Return final output, hidden state, and attention weights (for visualization)
+        return output, hidden, attn_weights
+    
+    
+## Filan ATT
+class LuongAttnDecoderRNN(nn.Module):
+    def __init__(self, attn_model, hidden_size, output_size, n_layers=1, dropout=0.1):
+        super(LuongAttnDecoderRNN, self).__init__()
+
+        # Keep for reference
+        self.attn_model = attn_model
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.n_layers = n_layers
+        self.dropout = dropout
+
+        # Define layers
+        self.embedding = nn.Embedding(output_size, hidden_size)
+        self.embedding_dropout = nn.Dropout(dropout)
+        self.gru = nn.GRU(hidden_size, hidden_size, n_layers, dropout=dropout)
+        self.concat = nn.Linear(hidden_size * 2, hidden_size)
+        self.out = nn.Linear(hidden_size, output_size)
+        
+        # Choose attention model
+        if attn_model != 'none':
+            self.attn = Attn(attn_model, hidden_size)
+
+    def forward(self, input_seq, last_hidden, encoder_outputs):
+        # Note: we run this one step at a time
+
+        # Get the embedding of the current input word (last output word)
+        batch_size = input_seq.size(0)
+        embedded = self.embedding(input_seq)
+        embedded = self.embedding_dropout(embedded)
+        embedded = embedded.view(1, batch_size, self.hidden_size) # S=1 x B x N
+
+        # Get current hidden state from input word and last hidden state
+        rnn_output, hidden = self.gru(embedded, last_hidden)
+
+        # Calculate attention from current RNN state and all encoder outputs;
+        # apply to encoder outputs to get weighted average
+        attn_weights = self.attn(rnn_output, encoder_outputs)
+        context = attn_weights.bmm(encoder_outputs.transpose(0, 1)) # B x S=1 x N
+
+        # Attentional vector using the RNN hidden state and context vector
+        # concatenated together (Luong eq. 5)
+        rnn_output = rnn_output.squeeze(0) # S=1 x B x N -> B x N
+        context = context.squeeze(1)       # B x S=1 x N -> B x N
+        concat_input = torch.cat((rnn_output, context), 1)
+        concat_output = F.tanh(self.concat(concat_input))
+
+        # Finally predict next token (Luong eq. 6, without softmax)
+        output = self.out(concat_output)
+
+        # Return final output, hidden state, and attention weights (for visualization)
+        return output, hidden, attn_weights
+    
+    
+### Testing the model
+small_batch_size = 3
+input_batches, input_lengths, target_batches, target_lengths = random_batch(small_batch_size)
+
+print('input_batches', input_batches.size()) # (max_len x batch_size)
+print('target_batches', target_batches.size()) # (max_len x batch_size)
+
+
+## Creating a model of smaller size
+small_hidden_size = 8
+small_n_layers = 2
+
+encoder_test = EncoderRNN(input_lang.n_words, small_hidden_size, small_n_layers)
+decoder_test = LuongAttnDecoderRNN('general', small_hidden_size, output_lang.n_words, small_n_layers)
+
+encoder_test.cuda()
+decoder_test.cuda()   
+
+
+encoder_outputs, encoder_hidden = encoder_test(input_batches, input_lengths, None)
+
+print('encoder_outputs', encoder_outputs.size()) # max_len x batch_size x hidden_size
+print('encoder_hidden', encoder_hidden.size()) # n_layers * 2 x batch_size x hidden_size
+
+
+max_target_length = max(target_lengths)
+
+# Prepare decoder input and outputs
+decoder_input = Variable(torch.LongTensor([SOS_token] * small_batch_size))
+decoder_hidden = encoder_hidden[:decoder_test.n_layers] # Use last (forward) hidden state from encoder
+all_decoder_outputs = Variable(torch.zeros(max_target_length, small_batch_size, decoder_test.output_size))
+
+if True:
+    all_decoder_outputs = all_decoder_outputs.cuda()
+    decoder_input = decoder_input.cuda()
+
+# Run through decoder one time step at a time
+for t in range(max_target_length):
+    decoder_output, decoder_hidden, decoder_attn = decoder_test(
+        decoder_input, decoder_hidden, encoder_outputs
+    )
+    all_decoder_outputs[t] = decoder_output # Store this step's outputs
+    decoder_input = target_batches[t] # Next input is current target
+
+# Test masked cross entropy loss
+loss = masked_cross_entropy(
+    all_decoder_outputs.transpose(0, 1).contiguous(),
+    target_batches.transpose(0, 1).contiguous(),
+    target_lengths
+)
+print('loss', loss.data[0])
